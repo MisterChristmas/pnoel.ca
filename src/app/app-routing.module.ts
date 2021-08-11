eebc078297ed1968779637bf9fbab231794fd7ac
Pascal <pascal.n@daillac.com>
2021-08-10 21:56:02 -0400

    Creation of skeleton for project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const routes: Routes = [
+  {
+    path: 'accueil',
+    component: NavigationComponent
+  },
+  {
+      path: '',
+      redirectTo: '/accueil',
+      pathMatch: 'full'
+  },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
